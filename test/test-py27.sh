b0ef7466895561b6c40cc74de604a9fc51431fbd
Mikko Värri <vmj@linuxbox.fi>
2017-08-15 19:59:53 +0300

    Use stretch instead of alpine in tests to catch more failures

--- test/test-py27.sh
+++ test/test-py27.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-alias py27='docker run --rm -it -v $(pwd):/src -w /src python:2-alpine sh ./test.sh'
+alias py27='docker run --rm -it -v $(pwd):/src -w /src python:2-stretch sh ./test/test.sh'
 
 py27 3.0.0
 py27 3.2.0
--- test/test-py3k.sh
+++ test/test-py3k.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-alias py3k='docker run --rm -it -v $(pwd):/src -w /src python:3-alpine sh ./test.sh'
+alias py3k='docker run --rm -it -v $(pwd):/src -w /src python:3-stretch sh ./test/test.sh'
 
 py3k 3.0.0
 py3k 3.2.0
